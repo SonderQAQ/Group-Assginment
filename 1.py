5764a2d41aa27dca7cfcc7212f16fca503c5df97
SonderQAQ <enderceeper01@gmail.com>
2025-12-12 15:57:40 +0800

    Update 1.py

--- 1.py
+++ 1.py
@@ -1,4 +1,11 @@
 import pandas as pd
 import numpy as np
-import streamlist as st
+import streamlit as st
 import os
+
+URL_DATA = 'https://storage.dosm.gov.my/gdp/gdp_annual_nominal_supply.parquet'
+
+df = pd.read_parquet(URL_DATA)
+if 'date' in df.columns: df['date'] = pd.to_datetime(df['date'])
+
+print(df)
